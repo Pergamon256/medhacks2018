87697b0d0e9bea559e56bcbcb8a1862d08b5f53a
Vincent Nagel <vnagel@umich.edu>
2018-09-08 11:24:08 -0400

    changed count thresholds, changed votebutton margins, renaming

--- postScript.js
+++ postScript.js
@@ -2,10 +2,10 @@ var voteCount = 0;
 function onUpvoteClick(){
     ++voteCount;
     document.getElementById('voteCount').innerHTML = voteCount;
-    if(voteCount >= 40){
+    if(voteCount >= 20){
         document.getElementById('voteCount').style.color = "green";
     }
-    else if(voteCount >= 20){
+    else if(voteCount >= 10){
         document.getElementById('voteCount').style.color = "blue";
     }
 }
