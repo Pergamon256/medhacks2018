7bc4bbe46df80e242344175f98c6246e92ea4603
Vincent Nagel <vnagel@umich.edu>
2018-09-08 10:57:41 -0400

    removed post downvote (we are going to reserve that functionality just for discussion). upvote working and changes votecount color depending on votecount

--- postScript.js
+++ postScript.js
@@ -2,6 +2,12 @@ var voteCount = 0;
 function onUpvoteClick(){
     ++voteCount;
     document.getElementById('voteCount').innerHTML = voteCount;
+    if(voteCount >= 40){
+        document.getElementById('voteCount').style.color = "green";
+    }
+    else if(voteCount >= 20){
+        document.getElementById('voteCount').style.color = "blue";
+    }
 }
 function onDownvoteClick(){
     --voteCount;
