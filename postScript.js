8a29587f232f8f378b71e0e691fc6e0d7fbcbd48
Vincent Nagel <vnagel@umich.edu>
2018-09-08 22:16:36 -0400

    worked on edit summary and added different pictures for gallery

--- postScript.js
+++ postScript.js
@@ -33,3 +33,14 @@ function onDownvoteClick(voteCountEl){
         document.getElementById(voteCountEl).style.color = "red";
     }
 }
+
+function onEditClick(){
+    sessionStorage.setItem('oldSummary',document.getElementById('summary').innerHTML.replace(/<(?:.|\n)*?>/gm, ''));
+    window.location.href="editExistingSummary.html";
+}
+
+function submitSummaryChanges(){
+    sessionStorage.setItem('newSummary',document.getElementById('newSummary').value);
+    window.location.href="post.html";
+}
+
